c8ba495e62b04e3288b6a1de85f98e982f0fa979
pitt0 <pitto.developer@gmail.com>
2023-12-02 22:37:16 +0100

    Day 1

---
+++ src/bin/day01.rs
@@ -0,0 +1,30 @@
+fn find_number(line: &str) -> u32 {
+    // this does not work if a string has no number
+    let d1 = line.chars().find(|c| c.is_numeric()).unwrap();
+    let d2 = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+    format!("{d1}{d2}").parse().unwrap()
+}
+
+
+fn find_and_replace_number(line: &str) -> u32 {
+    let line = line
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
+        .replace("zero", "z0o");
+    find_number(&line)
+}
+
+
+fn main() {
+    let input = std::fs::read_to_string("src/input01.txt").unwrap();
+    let sum: u32 = input.lines().map(find_and_replace_number).sum();
+
+    println!("{}", sum);
+}
