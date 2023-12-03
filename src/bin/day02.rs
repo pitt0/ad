6019bca459a61896f3c0ac34b4987c5a5358f370
pitt0 <pitto.developer@gmail.com>
2023-12-03 01:03:03 +0100

    Part 2 of day 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,8 +1,8 @@
 #[derive(Debug)]
 struct Round {
-    red_cubes: u8,
-    green_cubes: u8,
-    blue_cubes: u8
+    red_cubes: u64,
+    green_cubes: u64,
+    blue_cubes: u64
 }
 
 #[derive(Debug)]
@@ -11,7 +11,7 @@ struct Game {
     rounds: Vec<Round>
 }
 
-fn find_cubes_n(round: &str, color: &str) -> u8 {
+fn find_cubes_n(round: &str, color: &str) -> u64 {
     match round.split(", ").find(|s| { s.contains(color) }) {
         None => 0,
         Some(string) => string.chars().rev().skip(color.len() + 1).collect::<String>().chars().rev().collect::<String>().parse().unwrap()
@@ -43,19 +43,43 @@ fn form_games(line: &str) -> Game {
 
 fn main() {
     let input = std::fs::read_to_string("src/input02.txt").unwrap();
-    let sum = input
+    // Part 1
+    // let sum = input
+    //     .lines()
+    //     .map(form_games)
+    //     .filter(|g| {
+    //         match g.rounds
+    //             .iter()
+    //             .find(|r| { r.red_cubes > 12 || r.green_cubes > 13 || r.blue_cubes > 14 }) {
+    //                 None => true,
+    //                 _ => false
+    //             }
+    //     })
+    //     .map(|g| {
+    //         g.id
+    //     }).sum::<u32>();
+    
+    // Part 2
+    let sum: u64 = input
         .lines()
         .map(form_games)
-        .filter(|g| {
-            match g.rounds
-                .iter()
-                .find(|r| { r.red_cubes > 12 || r.green_cubes > 13 || r.blue_cubes > 14 }) {
-                    None => true,
-                    _ => false
-                }
-        })
         .map(|g| {
-            g.id
-        }).sum::<u32>();
+            g.rounds
+                .iter()
+                .map(|r| r.red_cubes)
+                .max()
+                .unwrap()
+            * g.rounds
+                .iter()
+                .map(|r| r.green_cubes)
+                .max()
+                .unwrap()
+            * g.rounds
+                .iter()
+                .map(|r| r.blue_cubes)
+                .max()
+                .unwrap()
+        })
+        .sum();
     println!("{sum}");
 }
