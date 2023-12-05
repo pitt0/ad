b7d5eb17d6d1242ca00d1d97fab7e852dbcb31ff
pitt0 <pitto.developer@gmail.com>
2023-12-05 02:00:31 +0100

    Day 04 Part 2 + some fixes/optimization

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,55 +1,74 @@
-// struct Card {
-//     id: u8,
-//     numbers: Vec<u8>,
-// }
-
-// fn get_id(game_name: &str) -> u8 {
-//     game_name
-//         .chars()
-//         .filter(|c| c.is_ascii_digit())
-//         .collect::<String>()
-//         .parse()
-//         .unwrap()
-// }
-
-fn get_winning_numbers(numbers: &str) -> Vec<u8> {
-    let cat: Vec<&str> = numbers.split('|').collect();
-    let wins: Vec<&str> = cat.first().unwrap().split(' ').collect();
-    let nums: Vec<&str> = cat.last().unwrap().split(' ').collect();
-    let mut numbers: Vec<u8> = Vec::new();
-
-    for win in wins {
-        if win.is_empty() {
-            continue;
-        }
-        if nums.contains(&win) {
-            numbers.push(win.parse().unwrap());
-        }
-    }
-    numbers
+struct Card {
+    _id: u8,
+    numbers: Vec<u8>,
+    winning: Vec<u8>,
 }
 
-fn get_value(line: &str) -> u32 {
+impl Card {
+    fn get_matches(&self) -> Vec<&u8> {
+        self.winning
+            .iter()
+            .filter(|win| self.numbers.contains(win))
+            .collect()
+    }
+}
+
+fn get_id(game_name: &str) -> u8 {
+    game_name
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<String>()
+        .parse()
+        .unwrap()
+}
+
+fn get_numbers_vec(s: &str) -> Vec<u8> {
+    s.split(' ')
+        .filter(|s| s.parse::<u8>().is_ok())
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn get_card(line: &str) -> Card {
     let game: Vec<&str> = line.split(':').collect();
-    // let card = Card {
-    //     id: get_id(game.first().unwrap()),
-    //     numbers: get_winning_numbers(game.first().unwrap()),
-    // };
-
-    let mut val: u32 = 0;
-    for _ in get_winning_numbers(game.last().unwrap()) {
-        if val == 0 {
-            val = 1;
-        } else {
-            val *= 2;
-        }
+    let cat: Vec<&str> = game.last().unwrap().split('|').collect();
+    Card {
+        _id: get_id(game.first().unwrap()),
+        numbers: get_numbers_vec(cat.last().unwrap()),
+        winning: get_numbers_vec(cat.first().unwrap()),
     }
-
-    val
 }
+
+// fn get_value(line: &str) -> u32 {
+//     let card: Card = get_card(line);
+
+//     let mut val: u32 = 0;
+//     for _ in card.get_matches() {
+//         if val == 0 {
+//             val = 1;
+//         } else {
+//             val *= 2;
+//         }
+//     }
+
+//     val
+// }
 
 fn main() {
     let input = std::fs::read_to_string("src/input04.txt").unwrap();
-    let sum: u32 = input.lines().map(get_value).sum();
-    println!("{sum}");
+
+    let cards: Vec<Card> = input.lines().map(get_card).collect();
+
+    let mut wins: Vec<u32> = vec![1; cards.len()];
+
+    for (index, card) in cards.iter().enumerate() {
+        for w in 0..card.get_matches().len() {
+            // if wins.get(index + w + 1).is_none() {
+            //     wins[index + w + 1] = 0
+            // }
+            let to_add = wins.get(index).unwrap().clone();
+            wins[index + w + 1] += to_add;
+        }
+    }
+    println!("{}", wins.iter().sum::<u32>());
 }
